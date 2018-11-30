efb7240505160dc72762bf833dc9d57cec82b724
jyp114110 <1140967775@qq.com>
2018-11-30 22:10:39 +0800

    商品管理模态框静态完成

--- public/back02/js/products.js
+++ public/back02/js/products.js
@@ -23,9 +23,9 @@ $(function(){
         $('#paginator').bootstrapPaginator({
           bootstrapMajorVersion: 3,
           currentPage: info.page,
-          totalPage : Math.ceil(info.total / info.size),
+          totalPages : Math.ceil(info.total / info.size),
           onPageClicked: function(a,b,c,page){
-            console.log(page);
+            // console.log(page);
             currentPage = page;
             render();
 
@@ -36,10 +36,21 @@ $(function(){
         
       }
     });
-  
-
   };
 
 
+  //  为 操作按钮 注册事件， 改变 商品的状态  ==> 没有提供 后端接口
+  // $('tbody').on('click','.btn',function(){
+  //   var id = $(this).parent().data("id");
+  //   // console.log(id);
+  // });
+
+  // 点击 添加商品按钮 显示 模态框
+  $('#addBtn').on('click',function(){
+    $('#productModal').modal('show');
+
+    // 发送 ajax，动态渲染
+
+  })
 
 })
